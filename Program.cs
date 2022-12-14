f5b66c87a204c781cfcf218c7fc7b249629810c1
Thork <Thork@DESKTOP-E5N1640>
2022-12-14 16:32:06 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Policy;
+
+namespace FinalFinalTriangleProject
+{
+
+    
+
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle();
+
+            //Welcomes the user
+            Console.WriteLine("Welcome to my triangle program!");
+            //Instructs the user about what the program will do
+            Console.WriteLine("This program will do many things like show you what type of triangle the sides you entered created.");
+            //Using a blank statement to create a line gap
+            Console.WriteLine("");
+
+            if(triangle.SideA == 0 || triangle.SideB == 0 || triangle.SideC == 0)
+            {
+                Console.WriteLine("automatically creating a new triangle since you dont have one already");
+                triangle.ValidateInput();
+            }
+
+            do
+            {
+                //Displays the menu of things the program can do
+                Console.WriteLine("Heres a menu of the things you can do");
+                string menuString = string.Format((" 1. Create a new triangle.\n 2. Display area and perimeter.\n 3. Classify triangle.\n 4. Reset triangle.\n 5. Save triangle to file.\n 6. Display file.\n 7. Exit"));
+                Console.WriteLine(menuString);
+
+                //Allows the user to select what they would like to do
+                Console.Write("Please select the number corresponding with what you would like to do:");
+
+                int userSelection = 0;
+                //Gets the input from the user and checks if its valid if not makes them select again
+                while (!int.TryParse(Console.ReadLine(), out userSelection)) // Execute while input is invalid
+                {
+                    Console.Clear();
+                    Console.WriteLine("That was not a number!");
+                    Console.Write("please enter a number that corresponds with the thing you would like to do: ");
+                }
+
+                
+
+                switch (userSelection)
+                {
+                    case 1:
+                        triangle = new Triangle();
+                        Console.Clear();
+                        triangle.ValidateInput();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        if (triangle.SideA != 0 || triangle.SideB != 0 || triangle.SideC != 0)
+                        {
+                            Console.WriteLine($"The perimeter of this triangle is {triangle.CalculatePerimeter()} and the area is {triangle.CalculateArea()}");
+                        }
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine($"This triangle is {triangle.ClassifyTriangle()}");
+                        break;
+                    case 4:
+                        Console.Clear();
+                        triangle = new Triangle();
+                        break;
+                    case 5:
+                        triangle.DisplayTriangle();
+                        Console.Clear();
+                        break;
+                    case 6:
+                        Console.Clear();
+                        triangle.ReadFile();
+                        break;
+                    case 7:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            while (true);
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalFinalTriangleProject
+{
+
+    internal class Triangle
+    {
+        double sideA, sideB, sideC, area, perimeter;
+        string triangleType;
+        public Triangle()
+        {
+
+        }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+        }
+
+        public double SideA
+        {
+            get { return sideA; }
+            set { sideA = value; }
+        }
+
+        public double SideB
+        {
+            get { return sideB; }
+            set { sideB = value; }
+        }
+
+        public double SideC
+        {
+            get { return sideC; }
+            set { sideC = value; }
+        }
+
+        public void ValidateInput()
+        {
+            
+            double sideA;
+            //Gets side A
+            Console.Write("Please enter side A of your triangle:");
+            while (!Double.TryParse(Console.ReadLine(), out sideA)) // Execute while input is invalid
+            {
+                Console.Clear();
+                Console.WriteLine("That was not a number!");
+                Console.Write("please enter a number for the length of side A: ");
+            }
+
+            double sideB;
+            //Gets side B
+            Console.Write("Please enter side B of your triangle:");
+            while (!Double.TryParse(Console.ReadLine(), out sideB)) // Execute while input is invalid
+            {
+                Console.Clear();
+                Console.WriteLine("That was not a number!");
+                Console.Write("please enter a number for the length of side B:");
+            }
+
+            double sideC;
+            //Gets side C
+            Console.Write("Please enter side C of your triangle:");
+            while (!Double.TryParse(Console.ReadLine(), out sideC)) // Execute while input is invalid
+            {
+                Console.Clear();
+                Console.WriteLine("That was not a number!");
+                Console.Write("please enter a number for the length of side C:");
+            }
+
+            this.sideC = sideC;
+            this.sideB = sideB;
+            this.sideA = sideA;
+        }
+
+        //Checks if the triangle is valid based on the triangle inequality theorem
+        public bool IsValidTriangle()
+        {
+            bool isValid = false;
+            if (sideA > 0 && sideB > 0 && sideC > 0)
+            {
+
+                //Checks if the triangle is valid based on the Triangle Inequality Theorem  
+                if (sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA)
+                {
+                    isValid = true;
+                }
+                else
+                {
+
+                    isValid = false;
+
+                }
+                
+            }
+
+            return isValid;
+        }
+
+        //calculates the area
+        public double CalculatePerimeter()
+        {
+            double perimeter = sideA + sideB + sideC;
+
+            this.perimeter = perimeter;
+            return perimeter;
+        }
+
+        //calculates the perimeter
+        public double CalculateArea()
+        {
+            double semiPerimeter = perimeter / 2;
+            double areaOfTriangle = Math.Sqrt(semiPerimeter * (semiPerimeter - this.sideA) * (semiPerimeter - this.sideB) * (semiPerimeter - this.sideC));
+
+            this.area = areaOfTriangle;
+            return areaOfTriangle;
+        }
+
+        //Classifies what type of triangle was entered
+        public string ClassifyTriangle()
+        {
+            string triangleType;
+            //Checks if the triangle is Equilateral
+            if (this.sideA == this.sideB && this.sideB == this.sideC)
+            {
+                triangleType = "Equilateral";
+            }
+            //Checkcs if the triangle is Isosceles
+            else if (this.sideA == this.sideB || this.sideA == sideC || this.sideB == this.sideC)
+            {
+                triangleType = "Isosceles";
+            }
+            //If its neither of those you can use logic to know that it has to be Scalene
+            else
+            {
+                triangleType = "Scalene";
+            }
+
+            this.triangleType = triangleType;
+            return triangleType;
+        }
+
+        
+        public void DisplayTriangle()
+        {
+            string fileName = "text.txt";
+            //Command that gets the current date
+            DateTime today = DateTime.Today;
+            ClassifyTriangle();
+            CalculatePerimeter();
+            CalculateArea();
+            string[] triangleInformation = new String[4];
+            triangleInformation[0] = $"This triangle was made {today.ToString("dd/MM/yyyy")}";
+            triangleInformation[1] = $"Ths sides of the triangle are the following A = {sideA} B = {sideB} C = {sideC}";
+            triangleInformation[2] = $"The perimeter of this triangle is {perimeter.ToString("0.##")} and the area is {area.ToString("0.##")}";
+            triangleInformation[3] = $"This triangle is {triangleType}";
+            File.WriteAllLines(fileName, triangleInformation);
+        }
+
+        public void ReadFile()
+        {
+            string fileName = "text.txt";
+            string[] lines = File.ReadAllLines(fileName);
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
